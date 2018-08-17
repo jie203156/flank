acd30b806cfa1deb839e77ae677b8085a144b1c4
zheng@xiaodong.com <zheng@xiaodong.com>
2018-08-18 03:41:40 +0800

    01

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
-$(function(){
+$(function() {
+
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
 
     $.ajax({
-       url:"http://mmb.ittun.com/api/getindexmenu",
-       dataType:"json",
-       success:function(result){
-           console.log(result);
-           
-            var html = template('getmenu',result);
+        url: "http://mmb.ittun.com/api/getindexmenu",
+        dataType: "json",
+        success: function(result) {
+            console.log(result);
+
+            var html = template('getmenu', result);
             $('.getindexmenu').html(html);
-            
-       } 
+
+        }
     })
 
 
-        
+
 
 
 })
