f2ede5231c91f1f174039fd0b89c6aba686df607
zheng@xiaodong.com <zheng@xiaodong.com>
2018-08-18 11:59:07 +0800

    完成超值折扣

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ $(function() {
         deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     });
 
+    //导航栏
     $.ajax({
         url: "http://mmb.ittun.com/api/getindexmenu",
         dataType: "json",
@@ -13,6 +14,18 @@ $(function() {
             var html = template('getmenu', result);
             $('.getindexmenu').html(html);
 
+        }
+    })
+
+    //商品列表
+    $.ajax({
+        url: "http://mmb.ittun.com/api/getmoneyctrl",
+        dataType: "json",
+        success: function(res) {
+            console.log(res);
+
+            var htmlstr = template("productlist", res);
+            $(".products ul").html(htmlstr);
         }
     })
 
