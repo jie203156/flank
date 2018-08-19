264d103a5ff85674e7e3f77dacf6152a79ab9466
ysl8888 <ysl88888999@163.com>
2018-08-19 19:16:55 +0800

    文件更新

--- js/pinpaidaquan1.js
+++ js/pinpaidaquan1.js
@@ -12,12 +12,49 @@ $(function () {
        }
    });
 
+
+   //  点击 品牌跳转
+   $('.brandname').on('click',"li",function () {
+      var id=$(this).data('id');
+       sessionStorage.setItem('ress',id);
+       window.location.href="pingpaidaquan2.html"
+
+   });
+
+
+
+    //  底部滚动到顶部动画
     var backButton=$('.back_to_top');
     function backToTop() {
         $('html,body').animate({
             scrollTop: 0
-        }, 4000);
+        }, 600);
     }
     backButton.on('click', backToTop);
+
+
+//    下载链接跳转
+    $('.header .img2').click(function () {
+        window.location="http://android.myapp.com/myapp/detail.htm?apkName=com.manmanbuy.bijia&ADTAG=mobile";
+    });
+
+
+//    登录界面
+    $('.login').click(function () {
+        window.location.href=""
+    })
+
+
+//    注册界面接口
+$('.register').click(function () {
+    window.location.href=""
+})
+
+
+
+//    logo跳转主页的接口
+    $('.header .img1').click(function () {
+        window.location.href="";
+    })
 
 });
