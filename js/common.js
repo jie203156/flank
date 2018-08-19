29d2b38ac9308fec814f8942a800c44f7ffe9e26
qianzhengkuan <40857634+qianzhengkuan@users.noreply.github.com>
2018-08-19 16:17:38 +0800

    完成返回顶部

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ $(function() {
     document.querySelector('html').style.fontSize = screen.width / 10 + 'px';
 
     //底部的返回顶部添加点击事件,需要给button加类setTop
-    $(".footer .setTop").click(function() {
+    $(".footer,.page .setTop").click(function() {
         $('body,html').animate({ scrollTop: 0 }, 500);
     })
 })
