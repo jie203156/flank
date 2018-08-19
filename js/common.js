a2d70e4f928d1842b19018093ce9ec4961f8ba6b
zheng@xiaodong.com <zheng@xiaodong.com>
2018-08-19 15:58:48 +0800

    首页完成

--- js/common.js
+++ js/common.js
@@ -1,4 +1,9 @@
-
+$(function() {
     //  rem模板
     document.querySelector('html').style.fontSize = screen.width / 10 + 'px';
 
+    //底部的返回顶部添加点击事件,需要给button加类setTop
+    $(".footer .setTop").click(function() {
+        $('body,html').animate({ scrollTop: 0 }, 500);
+    })
+})
