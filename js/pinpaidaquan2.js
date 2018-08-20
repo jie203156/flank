233587625e7e799eee9d48d0ae9030607b700884
ysl8888 <ysl88888999@163.com>
2018-08-20 15:04:08 +0800

    接口完善

--- js/pinpaidaquan2.js
+++ js/pinpaidaquan2.js
@@ -73,6 +73,41 @@ $(function () {
     })
 
 
+    //  底部滚动到顶部动画
+    var backButton=$('.back_to_top');
+    function backToTop() {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 600);
+    }
+    backButton.on('click', backToTop);
+
+
+//    下载链接跳转
+    $('.header .img2').click(function () {
+        window.location="http://android.myapp.com/myapp/detail.htm?apkName=com.manmanbuy.bijia&ADTAG=mobile";
+    });
+
+
+//    登录界面
+    $('.login').click(function () {
+        window.location.href="login.html"
+    })
+
+
+//    注册界面接口
+    $('.register').click(function () {
+        window.location.href="register.html"
+    })
+
+
+
+//    logo跳转主页的接口
+    $('.header .img1').click(function () {
+        window.location.href="index.html";
+    })
+
+
 
 
 
