3601ede050d882228e0a22f5c2c228c39d794cfe
ysl8888 <ysl88888999@163.com>
2018-08-20 08:43:57 +0800

    1

--- js/pinpaidaquan2.js
+++ js/pinpaidaquan2.js
@@ -1,38 +1,25 @@
 $(function () {
+    rander2(0);
+    rander1(0);
+    rander(3);
+
+
     var uid=sessionStorage.getItem("ress");
     sessionStorage.removeItem('ress');
     console.log(uid);
     var id=Number(uid);
+    rander2(id);
+    rander1(id);
 
-
-    $.ajax({
-        url:"http://mmb.ittun.com/api/getbrand",
-        type:"get",
-        dataType:"json",
-        data:{brandtitleid:id},
-        success:function (res) {
-            console.log(res);
-            var htmlstr=template('mmp',res)
-            $('.zhanshi').html(htmlstr);
-
-        }
-    });
-
-    $.ajax({
-        url:"http://mmb.ittun.com/api/getbrandproductlist",
-        type:'get',
-        data:{brandtitleid:id,pagesize:10},
-        success:function (res) {
-            console.log(res);
-            htmlstr=template("panghang",res);
-            $('.content2').html(htmlstr);
-
-        }
-    })
 
     $('.content2').on('click','.pinpaiming',function () {
         var ids=$(this).data('id');
         var id=Number(ids);
+       rander(id);
+
+    });
+
+    function rander(id) {
         $.ajax({
             url:"http://mmb.ittun.com/api/getproductcom",
             type:"get",
@@ -43,8 +30,37 @@ $(function () {
                 $('.userconmet').html(htmlstr);
             }
         })
+    }
 
-    })
+    function rander1(id) {
+        $.ajax({
+            url:"http://mmb.ittun.com/api/getbrandproductlist",
+            type:'get',
+            data:{brandtitleid:id,pagesize:10},
+            success:function (res) {
+                console.log(res);
+                htmlstr=template("panghang",res);
+                $('.content2').html(htmlstr);
+
+            }
+        });
+    }
+
+    function rander2(id) {
+        $.ajax({
+            url:"http://mmb.ittun.com/api/getbrand",
+            type:"get",
+            dataType:"json",
+            data:{brandtitleid:id},
+            success:function (res) {
+                console.log(res);
+                var htmlstr=template('mmp',res)
+                $('.zhanshi').html(htmlstr);
+
+            }
+        });
+
+    }
 
 
 
